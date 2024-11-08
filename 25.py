46e4315812bb6df9566fb50eb8d408febab6d6d1
hijas-codes <149758697+hijas-codes@users.noreply.github.com>
2024-11-08 15:34:44 +0530

    Add files via upload

---
+++ 25.py
@@ -0,0 +1,12 @@
+student={}
+ls=[]
+n=int(input("enter no. of students  :"))
+for i in range(0,n):
+	name=input("enter name		:")
+	age=input("enter age		:")
+	grade=input("enter grade	:")
+	student[name]=age,grade
+	print("\n")
+print(student)
+	
+
